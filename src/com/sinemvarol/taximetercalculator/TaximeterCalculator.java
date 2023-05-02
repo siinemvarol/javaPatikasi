0973d04b54895d52300be08dbd529e0121a914e4
siinemvarol <siinemvarol@gmail.com>
2023-05-02 20:59:49 +0300

    Taximeter Calculator

--- src/com/sinemvarol/taximetercalculator/TaximeterCalculator.java
+++ src/com/sinemvarol/taximetercalculator/TaximeterCalculator.java
@@ -1,4 +1,25 @@
 package com.sinemvarol.taximetercalculator;
 
+import java.util.Scanner;
+
 public class TaximeterCalculator {
+
+    public static void main(String[] args) {
+
+        int km;
+        double perKm = 2.20;
+        double startingPrice = 10;
+        double total = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter distance in KM: ");
+        km = input.nextInt();
+
+        total += (km * perKm) + startingPrice;
+
+        total = (total < 20) ? 20 : total;
+
+        System.out.println("Total price is: " + total);
+
+    }
 }
