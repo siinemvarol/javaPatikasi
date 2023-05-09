55bc0f5c02412bdb68c941a0a4e7b10e4a13f44b
siinemvarol <siinemvarol@gmail.com>
2023-05-09 15:02:24 +0300

    Activity Recommendation

---
+++ src/com/sinemvarol/activityrecommendation/ActivityRecommendation.java
@@ -0,0 +1,28 @@
+package com.sinemvarol.activityrecommendation;
+
+import java.util.Scanner;
+
+public class ActivityRecommendation {
+
+    public static void main(String[] args) {
+
+        int temperature;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter air temperature: ");
+        temperature = scanner.nextInt();
+
+        if(temperature < 5){
+            System.out.println("Skiing");
+        } else if (temperature >= 5 && temperature <= 25){
+            if(temperature <= 15){
+                System.out.println("Going to the movies");
+            } else {
+                System.out.println("Picnic");
+            }
+        } else {
+            System.out.println("Swimming");
+        }
+    }
+}
